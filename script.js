f0ada2aa6612c016b62d85af700c8d06546d0ed9
Izabelle Avena <93957235+Izabelleavena@users.noreply.github.com>
2022-08-03 13:40:18 -0300

    Uploud documentos

---
+++ script.js
@@ -0,0 +1,34 @@
+
+  
+// Use the if condition in case any browser
+// does not support canvas
+function funcao() {
+    document.getElementById("aboutMe").innerHTML   = "About Me";
+    document.getElementById("ability").innerHTML   = "Ability";
+    document.getElementById("project").innerHTML   = "Project";
+    document.getElementById("contact").innerHTML   = "Contact";
+    document.getElementById("intro").innerHTML     = "Front-end developer, ABAP and FIORI/UI5";
+    document.getElementById("paragrafo").innerHTML = "Born in Rio, carioca da gem, with a capixaba touch. Lived in Vila Velha for 6 years. physical educator, who she decided to change her life and is now exploring the world of technology, a front-end apprentice, playing in the world of ABAP and FIORI/UI5 development, different worlds that lie ahead. Future full stack? Who knows. My name is Izabelle Avena, I'm 26 years old, determined, I love working in a team, and I always give 100% of myself.";
+    document.getElementById("myskils").innerHTML   = "Today my softskils are focused on front-end knowledge, where I study the use of tools like HTML:5, CSS:3, JAVASCRIPT, BOOTSTRAP, GIT, FIGMA, as well as frameworks like VUE.JS. I study and work today with the ABAP language with back-end, and with FIORI/UI5 in the front-end area.";
+    document.getElementById("myability").innerHTML = "My skills";
+    document.getElementById("soft").innerHTML      = "A little more about me...";
+    document.getElementById("paragrafo_dois").innerHTML = "Excited who loves a relaxed place, motivated, who runs after goals, totally focused on giving the best of me to achieve my dreams, with focus, punctuality, and responsibility. I love working in a team, talking and communicating with people, I'm very good at respecting hierarchy and striving to be able to stand out.";
+    document.getElementById("animada").innerHTML = "Lively/Fun";
+    document.getElementById("focada").innerHTML = "Focused";
+    document.getElementById("pontual").innerHTML = "One-off";
+    document.getElementById("criativa").innerHTML = "Creative";
+    document.getElementById("determinada").innerHTML = "Determined";
+    document.getElementById("myproject").innerHTML = "My projects";
+    document.getElementById("homenagem").innerHTML = "Netflix tribute";
+    document.getElementById("conversor").innerHTML = "Currency converter";
+    document.getElementById("catalogoCel").innerHTML = "Catalog: cell phone sales";
+    document.getElementById("relogio").innerHTML = "Catalog: Watch sale website";
+    document.getElementById("cadastro").innerHTML = "Registration in JAVASCRIPT";
+    document.getElementById("site").innerHTML = "Complete business website";
+    document.getElementById("filmes").innerHTML = "Favorite Movies List";
+    document.getElementById("triangulo").innerHTML = "Discover the triangle";
+    document.getElementById("contactme").innerHTML = "Contact";
+    document.getElementsByName('name')[0].placeholder='Name';
+    document.getElementsByName('message')[0].placeholder='Your message';
+    document.getElementsByName('enviar')[0].value='Submit';
+}
